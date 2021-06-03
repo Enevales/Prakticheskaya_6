a8604471fdcb288cd2571496ec28f6c62b23bb93
Enevales <78997384+Enevales@users.noreply.github.com>
2021-06-03 23:53:50 +0300

    Add files via upload

---
+++ ex4.cpp
@@ -0,0 +1,33 @@
+/* Написать функцию без параметров, возвращающую при каждом
+вызове следующее простое число (первый вызов возвращает 2,
+следующий — 3, третий — 5, и т. д.).*/
+
+#include <iostream>  
+
+using  namespace std;
+
+int func(int n) 
+{  
+    int a, b;
+ 
+    for ( a = n+1; ; a++) 
+    { 
+        for ( b = 2; b <= ( a / b); b++) 
+            if(!( a % b)) 
+                break;  
+        if( b > ( a / b)) 
+               break; 
+    } 
+ 
+    return a;
+}
+ 
+int main() 
+{
+    for(int i=1; i<=17; i++){
+        if(func(i)>func(i-1)){
+            cout<<func(i)<<endl;
+        }
+    }
+    return 0;
+}
